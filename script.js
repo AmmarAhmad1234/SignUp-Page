0c527c7b32b29eb7c1aabc60b2d66578bfbcb67d
Ammar <ammarfarman93@gmail.com>
2024-08-11 12:20:19 -0700

    donee

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
+
 let SignUpBtn = document.getElementById('SignUpBtn');
 
-// console.log(SignUpBtn)
+// console.log(SignUpBtn);
 
-SignUpBtn.addEventListener('click', (a)=>{
+const userArray = [];
 
-a.preventDefault();
+SignUpBtn.addEventListener('click',(e)=>{
+    e.preventDefault();
 
 
 let UserName = document.getElementById('UserName').value;
@@ -16,7 +18,8 @@ let Gender = document.getElementById('Gender').value;
 let Zip = document.getElementById('Zip').value;
 let Date = document.getElementById('Date').value;
 
-let UserObj = {
+
+let UserObj ={
 UserName,
 Email,
 Password,
@@ -27,8 +30,25 @@ Zip,
 Date
 }
 
+// console.log(UserObj);
+userArray.push(UserObj);
+console.log(userArray)
+
+let Userstringify =JSON.stringify(userArray);
+// console.log(Userstringify)
+localStorage.setItem('UserData',Userstringify);
+
+let reset = document.getElementById("SignUpForm").reset();
+
+let SuccessMessage = document.getElementById('SuccessMessage');
+
+SuccessMessage.innerHTML = `<div class="alert alert-warning alert-dismissible fade show" role="alert">
+  <strong>Success Registered</strong>
+  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+</div>`
 
 
-console.log(UserObj)
+
+
 
 })
